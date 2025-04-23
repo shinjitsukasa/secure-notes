92355a23b009a2482452a3133c4e7e3490268e5c
shinjitsukasa <shinjitsukasa09@gmail.com>
2025-04-23 16:55:18 +0800

    some securityconfig changes, create in memory accounts for testing

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -4,29 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests)
+                -> requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        //http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
 
-	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http
-				.authorizeRequests((requests) -> requests
-						.requestMatchers("/contact").permitAll()
-						.requestMatchers("/api/notes/**").permitAll()
-						.requestMatchers("/public/**").permitAll()
-						.requestMatchers("/admin/**").denyAll()
-						.anyRequest().authenticated());
-		// http.formLogin(withDefaults());
-		http.csrf(csrf -> csrf.disable());
-		http.sessionManagement(session -> session
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		http.httpBasic(withDefaults());
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager =
+                new InMemoryUserDetailsManager();
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin2")) {
+            manager.createUser(
+                    User.withUsername("admin2")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
+    }
 
-		return http.build();
-	}
 }
