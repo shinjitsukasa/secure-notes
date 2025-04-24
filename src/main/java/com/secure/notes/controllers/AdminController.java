4162cdb2e4bb6f670b2c12f459679c0ffbe12142
shinjitsukasa <shinjitsukasa09@gmail.com>
2025-04-25 07:25:30 +0800

    added bcrypt

--- src/main/java/com/secure/notes/controllers/AdminController.java
+++ src/main/java/com/secure/notes/controllers/AdminController.java
@@ -3,15 +3,18 @@ package com.secure.notes.controllers;
 import com.secure.notes.dtos.UserDTO;
 import com.secure.notes.models.User;
 import com.secure.notes.services.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+// @PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
     @Autowired
--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.secure.notes.models.AppRole;
@@ -21,16 +23,26 @@ import org.springframework.boot.CommandLineRunner;
 
 @Configuration
 @EnableWebSecurity
+// @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests)
-                -> requests.anyRequest().authenticated());
+                -> requests
+					 .requestMatchers("/api/admin/**").hasRole("ADMIN")
+					 .requestMatchers("/api/notes/**").hasAnyRole("USER", "ADMIN")
+					 .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 	 @Bean
     public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
